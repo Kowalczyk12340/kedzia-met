7532ed593a252647ecca68964a2d904742ea853f
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2020-05-26 21:51:21 +0200

    Updated Landing component

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../../images/kedzia.png";
+
+
+const LandingContainer = styled.div `
+    .logotyp
+    {
+      height: 24px;
+      width: auto;
+    }
+    
+`;
 
 export const Landing = () => {
-  return <div>strona glowna</div>;
+  return <LandingContainer >
+      <img className = "logotyp" src = {logo} / >
+    </LandingContainer>;
 };
