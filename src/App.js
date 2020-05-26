00ae077adde0af15ddbfd9a86e582109d9e158bc
root <root@pc-718.home>
2020-05-26 21:24:23 +0200

    added Landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Navbar} from './components/layout/Navbar';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navbar } from "./components/layout/Navbar";
+import { Landing } from "./components/landing/Landing";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
       </Router>
     </div>
   );
