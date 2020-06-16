e7b5c542a6830c25e9437ab165c3d08e87d272ba
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2020-06-16 21:58:22 +0200

    change in photogallery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navbar } from "./components/layout/Navbar";
 import { Landing } from "./components/landing/Landing";
